2a1aa6c61e71e589986e8b01b7b590d0a9c6ce75
AlexMorrison <amo37066@uga.edu>
2015-10-24 14:56:33 -0400

    changed arithmetic

--- Player.py
+++ Player.py
@@ -128,7 +128,7 @@ class Player:
     def giveExperiencePoints(self, pointsToGive):
         self.experiencePoints += pointsToGive
 
-        if self.experiencePoints % 100 == 0:
+        if self.experiencePoints / 100 == 0:
             self.levelUp()
 
     """
