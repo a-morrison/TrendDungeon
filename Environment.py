6afe5b1259b7246648df5a5560cfe868648cd1d1
AlexMorrison <amo37066@uga.edu>
2015-10-24 12:19:31 -0400

    updated environment

--- Environment.py
+++ Environment.py
@@ -2,20 +2,40 @@
     "This class represents the environment for the game
     "Trend Dungeon
 """
+import random
 from Scenario import Scenario
+from Creature import Creature
 
-locations = ['Cave', 'Crypt', 'Dark Forest', 'Swamp', 'Canyon','Cliff', 'Witche\'s Hut', 'Shoppe']
+locations = ['Witch Hut', 'Cave', 'Crypt', 'Dark Forest', 'Swamp', 'Canyon', 'Cliff', 'Shoppe']
+jsonPath = './json/events.json'
 
 class Environment:
+    location = ''
+    generalText = ""
+    newScenario = None
+    creature = None 
 
     def __init__(self):
+        newScenario = ""
 
     def loadScenario(self, scenario):
 
     def saveScenario(self, scenario):
 
     def generateScenario(self, trend):
-        
+        self.location = random.choice(locations)
+        self.getGeneralText(trend)
+
+    def getGeneralText(self, trend):
+        with open(jsonPath, "r") as data_file:
+            data = json.load(data_file)
+
+        generalText = data[self.location][0]["generalText"]
+        self.generalText = generalText.format(trend)
+
+    def spawCreature(self):
+        self.creature = Creature()
+
     def getNoun(self, trend):
         trendList = list(trend)
         for x in trendList:
