d6aac78d07b8ade698f19e985da299d5b5f35a1b
AlexMorrison <amo37066@uga.edu>
2015-10-23 22:21:38 -0400

    updated Player

--- Player.py
+++ Player.py
@@ -2,19 +2,59 @@
   "This class represents a player for the game
   "Trend Dungeon
 """
+
+import json
+
 class Player:
-    level = 1
-    health = 10
+    level = 0
+    health = 0
     experiencePoints = 0
-    damage = 1
+    damage = 0
 
     def __init__(self):
+        self.loadPlayer()
+
+    def loadPlayer(self):
+        with open("./json/player.json", "r") as data_file:
+            data = json.load(data_file)
+
+        level = data["Player"][0]["level"]
+        self.level = int(level)
+
+        health = data["Player"][0]["health"]
+        self.health = int(health)
+
+        experiencePoints = data["Player"][0]["experiencePoints"]
+        self.experiencePoints = int(experiencePoints)
+
+        damage = data["Player"][0]["damage"]
+        self.damage = int(damage)
     
+    def savePlayer(self):
+        with open("./json/player.json", "r") as data_file:
+            data = json.load(data_file)
+
+        tmp = data["Player"][0]["level"]
+        data["Player"][0]["level"] = str(self.level)
+
+        tmp = data["Player"][0]["health"]
+        data["Player"][0]["health"] = str(self.health)
+
+        tmp = data["Player"][0]["experiencePoints"]
+        data["Player"][0]["experiencePoints"] = str(self.experiencePoints)
+
+        tmp = data["Player"][0]["damage"]
+        data["Player"][0]["damage"]
+
+        with open("./json/player.json", "w") as data_file:
+            data_file.write(json.dumps(data, indent=4, 
+                            separators=(', ', ': ')))
+
     """
       "Returns the health of the player.
     """
     def getHealth(self):
-        return health
+        return self.health
     
     """
       "Removes a supplied number of health
@@ -40,7 +80,7 @@ class Player:
       "Returns the damage amount of the player.
     """
     def getDamage(self):
-        return damage
+        return self.damage
     
     """
       "Gives the player a supplied number of
@@ -58,7 +98,11 @@ class Player:
     """
       "Levels up the player.
     """
-    def levelUp(self)
+    def levelUp(self):
         self.level += 1
 
-    
+    def isDead(self):
+        if health <= 0:
+            return true
+        else:
+            return false
